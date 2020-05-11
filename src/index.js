fd96af176f4e18e5685686fe698eefd7e3a869dd
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-11 10:15:31 +0100

    service

--- src/index.js
+++ src/index.js
@@ -101,8 +101,3 @@ class Container extends React.Component {
 
 const domContainer = document.querySelector('#root');
 ReactDOM.render(<Container />, domContainer);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
