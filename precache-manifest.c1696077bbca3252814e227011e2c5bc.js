2cc6aaa8a3ed3ba5f4af85797c6961b779f618ac
Bojo <bozhidar_zahariev@hotmail.com>
2020-05-22 09:50:17 +0100

    Updates

---
+++ precache-manifest.c1696077bbca3252814e227011e2c5bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b7d611b38771658f43aab69801be67",
+    "url": "/Ruin_My_Lunch/index.html"
+  },
+  {
+    "revision": "3440a678ee4d3fbb1f38",
+    "url": "/Ruin_My_Lunch/static/css/main.5a108803.chunk.css"
+  },
+  {
+    "revision": "c4ca7a55d2b819e7909e",
+    "url": "/Ruin_My_Lunch/static/js/2.3e4b8e55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ruin_My_Lunch/static/js/2.3e4b8e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3440a678ee4d3fbb1f38",
+    "url": "/Ruin_My_Lunch/static/js/main.695828f0.chunk.js"
+  },
+  {
+    "revision": "6e1e26361dd381aaec2c",
+    "url": "/Ruin_My_Lunch/static/js/runtime-main.108f5f7f.js"
+  },
+  {
+    "revision": "79935d5160be4ad09e8e916c3672e833",
+    "url": "/Ruin_My_Lunch/static/media/leg.79935d51.png"
+  },
+  {
+    "revision": "723b6afa6fe2cfa1ed5f5256446bfc28",
+    "url": "/Ruin_My_Lunch/static/media/plate.723b6afa.png"
+  }
+]);
